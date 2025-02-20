89bd61f8bc569c144d63c9f641b2b1bc5a614b6b
Dumgrammer <bastianlacap55@gmail.com>
2025-02-21 01:05:23 +0800

    Dashboard activity

--- Djangotest/Djangotest/urls.py
+++ Djangotest/Djangotest/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', include('portfolio.urls', namespace="portfolio"))
+    path('portfolio/', include('portfolio.urls', namespace="portfolio")),
+    path('dashboard/', include('dashboard.urls', namespace="dashboard"))
 ]
